829c96569946a1cb6963c1de8996aca00544971a
stla <laurent_step@yahoo.fr>
2019-09-14 15:33:33 +0200

    first commit

---
+++ R/hypergeomPFQ.R
@@ -0,0 +1,133 @@
+#' Hypergeometric function of a matrix argument
+#'
+#' @description Evaluates a truncated hypergeometric function of a matrix
+#' argument.
+#'
+#' @param m truncation weight, a positive integer
+#' @param a the "upper" parameters, a numeric or complex vector,
+#' possibly empty (or \code{NULL})
+#' @param b the "lower" parameters, a numeric or complex vector,
+#' possibly empty (or \code{NULL})
+#' @param x either a real symmetric matrix, a Hermitian complex matrix,
+#' or a numeric or complex vector, the eigen values of the matrix
+#' @param alpha the alpha parameter, a positive number
+#'
+#' @return A real or a complex number.
+#' @export
+#'
+#' @references Koev, P. and Edelman, A. (2006).
+#' \emph{The Efficient Evaluation of the Hypergeometric Function of a Matrix Argument}.
+#' Mathematics of Computation, 75, 833-846, 2006.
+#'
+#' @examples # a single x example, the Gauss hypergeometric function
+#' hypergeomPFQ(m = 20, a = c(1,2), b = c(3), x = 0.5)
+#' gsl::hyperg_2F1(1, 2, 3, 0.5)
+#' # 0F0 is the exponential of the trace
+#' X <- toeplitz(c(3,2,1))/10
+#' hypergeomPFQ(m = 10, a = NULL, b = NULL, x = X)
+#' exp(sum(diag(X)))
+#' # 1F0 is det(I-X)^(-a)
+#' X <- toeplitz(c(3,2,1))/100
+#' hypergeomPFQ(m = 15, a = 3, b = NULL, x = X)
+#' det(diag(3)-X)^(-3)
+#' # Herz's relation for 1F1
+#' hypergeomPFQ(m=15, a = 2, b = 3, x = X)
+#' exp(sum(diag(X))) * hypergeomPFQ(m=15, a = 3-2, b = 3, x = -X)
+#' # Herz's relation for 2F1
+#' hypergeomPFQ(15, a = c(1,2), b = 3, x = X)
+#' det(diag(3)-X)^(-2) *
+#'   hypergeomPFQ(15, a = c(3-1,2), b = 3, -X%*%solve(diag(3)-X))
+hypergeomPFQ <- function(m, a, b, x, alpha = 2){
+  stopifnot(
+    m >= 1,
+    is.null(a) || is.vector(a),
+    is.null(b) || is.vector(b),
+    alpha > 0
+  )
+  if(is.matrix(x)){
+    stopifnot(isSymmetric(x))
+    x <- eigen(x, symmetric = TRUE, only.values = TRUE)$values
+  }else{
+    stopifnot(is.vector(x))
+  }
+  if(all(x == x[1L])){
+    return(HypergeoI(m, alpha, a, b, length(x), x[1L]))
+  }
+  #
+  summation <- function(i, z, j){
+    jack <- function(k, beta, c, t, mu, Nmu){
+      for(i in max(k,1L) : sum(mu > 0L)){
+        if(length(mu) == i || mu[i] > mu[i+1L]){
+          d <- Nmu
+          gamma <- beta * .betaratio(kappa, mu, i, alpha)
+          mu[i] <- mu[i]-1L
+          Nmu <- .Nkappa(mu) - 1L
+          if(mu[i] > 0L){
+            jack(i, gamma, c+1L, t, mu, Nmu)
+          }else{
+            if(Nkappa > 1L){
+              J[Nkappa,t] <<- J[Nkappa,t] + gamma*x[t]^(c+1L) *
+                ifelse(any(mu>0L), J[Nmu,t-1L], 1) #any(mu>0L) <=> mu[1]>0L ?
+            }
+          }
+          mu[i] <- mu[i] + 1L
+          Nmu <- d
+        }
+      }
+      if(k == 0L){
+        if(Nkappa>1L) J[Nkappa,t] <<- J[Nkappa,t] + J[Nkappa,t-1L]
+      }else{
+        J[Nkappa,t] <<- J[Nkappa,t] + beta * x[t]^c * J[Nmu,t-1L]
+      }
+    }
+    # end jack
+    r <- if(i == 1L) j else min(kappa[i-1L],j)
+    for(kappai in seq_len(r)){
+      kappa[i] <<- kappai
+      Nkappa <- .Nkappa(kappa) - 1L
+      z <- z * .T(alpha, a, b, kappa, i)
+      if(Nkappa > 1L && (length(kappa) == 1L || kappa[2L] == 0L)){
+        J[Nkappa,1L] <<- x[1L]*(1+alpha*(kappa[1L]-1L)) * J[Nkappa-1L,1L]
+      }
+      for(t in 2L:n) jack(0L, 1, 0L, t, kappa, Nkappa)
+      s <<- s + z * J[Nkappa,n]
+      if(j > kappai && i < n){
+        summation(i+1L, z, j-kappai)
+      }
+    }
+    kappa[i] <<- 0L
+  } # end summation
+  #
+  #
+  n <- length(x)
+  Pmn <- .P(m,n)
+  s <- 1
+  J <- matrix(0, Pmn, n)
+  J[1L,] <- cumsum(x)
+  kappa <- integer(0L)
+  #
+  D <- rep(NA_integer_, Pmn)
+  Last <- t(as.integer(c(0,m,m)))
+  fin <- 0L
+  for(. in 1L:n){
+    NewLast <- matrix(NA_integer_, nrow = 0L, ncol = 3L)
+    for(i in 1L:nrow(Last)){
+      manque <- Last[i,2L]
+      l <- min(manque, Last[i,3L])
+      if(l > 0L){
+        D[Last[i,1L]+1L] <- fin + 1L
+        s <- 1L:l
+        NewLast <- rbind(NewLast, cbind(fin+s, manque-s, s))
+        fin <- fin + l
+      }
+    }
+    Last <- NewLast
+  }
+  .Nkappa <- function(kappa){
+    kappa <- kappa[kappa>0L]
+    if(length(kappa) == 0L) return(1L)
+    D[.Nkappa(head(kappa,-1L))] + tail(kappa,1L)
+  }
+  summation(1L, 1, m)
+  s
+}
---
+++ R/internal.R
@@ -0,0 +1,60 @@
+#' @importFrom arrangements npartitions
+NULL
+
+.P <- function(m, n){
+  sum(sapply(1L:m, function(i){
+    sum(sapply(1L:min(m,n,i), function(k){
+      npartitions(i, k)
+    }))
+  }))
+}
+
+.T <- function(alpha, a, b, kappa, i){
+  if(length(kappa) == 0L || kappa[1L] == 0L) return(1)
+  c <- kappa[i] - 1L - (i-1L)/alpha
+  d <- kappa[i]*alpha - i
+  s <- seq_len(kappa[i]-1L)
+  e <- d - s*alpha + vapply(s, function(i) sum(kappa >= i), integer(1L))
+  g <- e + 1
+  s <- seq_len(i-1L)
+  f <- kappa[s]*alpha - s - d
+  h <- f + alpha
+  l <- h*f
+  prod1 <- prod(a+c)
+  prod2 <- prod(b+c)
+  prod3 <- prod((g-alpha)*e / (g*(e+alpha)))
+  prod4 <- prod((l-f) / (l+h))
+  out <- prod1/prod2 * prod3 * prod4
+  return(ifelse(is.nan(out) || is.infinite(out), 0, out))
+}
+
+.betaratio <- function(kappa, mu, k, alpha){
+  t <- k - alpha*mu[k]
+  s <- seq_len(k)
+  u <- t + 1 - s + alpha*kappa[s]
+  s <- seq_len(k-1L)
+  v <- t - s + alpha*mu[s]
+  s <- seq_len(mu[k]-1L)
+  w <- vapply(s, function(i) sum(mu >= i), integer(1L)) - t - alpha*s
+  alpha * prod(u/(u+alpha-1)) * prod((v+alpha)/v) * prod((w+alpha)/w)
+}
+
+HypergeoI <- function(m, alpha, a, b, n, x){
+  summation <- function(i, z, j){
+    for(kappai in seq_len(if(i==1L) j else min(kappa[i-1L],j))){
+      kappa[i] <<- kappai
+      t <- .T(alpha, a, b, kappa, i)
+      z <- z * x * (n-i+1L+alpha*(kappai-1L)) * t
+      s <<- s + z
+      if(j > kappai && i < n){
+        summation(i+1L, z, j-kappai)
+      }
+    }
+    kappa[i] <<- 0L
+  }
+  s <- 1
+  kappa <- integer(0L)
+  summation(1L, 1, m)
+  s
+}
+
