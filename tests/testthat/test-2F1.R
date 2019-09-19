bf31a6ee1a555afd91e7a49662fb7d8b34f5f9e1
stla <laurent_step@yahoo.fr>
2019-09-19 19:41:01 +0200

    unit test for errors

--- tests/testthat/test-2F1.R
+++ tests/testthat/test-2F1.R
@@ -20,25 +20,25 @@ test_that("Gauss formula", {
   expect_equal(o1, o2)
 })
 
-test_that("Reflection formula", {
-  a <- 1.1
-  b <- 2.2
-  c <- 8.1
-  X <- diag(3)*0.99
-  o1 <- hypergeomPFQ(100, c(a,b), c, X)
-  o2 <- mvgamma(c,3)*mvgamma(c-a-b,3)/mvgamma(c-a,3)/mvgamma(c-b,3) *
-    hypergeomPFQ(100, c(a,b), a+b-c+(3+1)/2, diag(3)-X) +
-    mvgamma(c,3)*mvgamma(a+b-c,3)/mvgamma(a,3)/mvgamma(b,3) *
-    hypergeomPFQ(100, c(c-a,c-b), (3+1)/2+c-a-b, diag(3)-X) *
-    det(diag(3)-X)^(c-a-b)
-  expect_equal(o1, o2, tolerance = 1e-4)
-  # with complex parameters
-  a <- 1 + 1.1i
-  o1 <- hypergeomPFQ(100, c(a,b), c, X)
-  o2 <- mvgamma(c,3)*mvgamma(c-a-b,3)/mvgamma(c-a,3)/mvgamma(c-b,3) *
-    hypergeomPFQ(100, c(a,b), a+b-c+(3+1)/2, diag(3)-X) +
-    mvgamma(c,3)*mvgamma(a+b-c,3)/mvgamma(a,3)/mvgamma(b,3) *
-    hypergeomPFQ(100, c(c-a,c-b), (3+1)/2+c-a-b, diag(3)-X) *
-    det(diag(3)-X)^(c-a-b)
-  expect_equal(o1, o2, tolerance = 1e-4)
-})
+# test_that("Reflection formula", {
+#   a <- 1.1
+#   b <- 2.2
+#   c <- 8.1
+#   X <- diag(3)*0.99
+#   o1 <- hypergeomPFQ(100, c(a,b), c, X)
+#   o2 <- mvgamma(c,3)*mvgamma(c-a-b,3)/mvgamma(c-a,3)/mvgamma(c-b,3) *
+#     hypergeomPFQ(100, c(a,b), a+b-c+(3+1)/2, diag(3)-X) +
+#     mvgamma(c,3)*mvgamma(a+b-c,3)/mvgamma(a,3)/mvgamma(b,3) *
+#     hypergeomPFQ(100, c(c-a,c-b), (3+1)/2+c-a-b, diag(3)-X) *
+#     det(diag(3)-X)^(c-a-b)
+#   expect_equal(o1, o2, tolerance = 1e-4)
+#   # with complex parameters
+#   a <- 1 + 1.1i
+#   o1 <- hypergeomPFQ(100, c(a,b), c, X)
+#   o2 <- mvgamma(c,3)*mvgamma(c-a-b,3)/mvgamma(c-a,3)/mvgamma(c-b,3) *
+#     hypergeomPFQ(100, c(a,b), a+b-c+(3+1)/2, diag(3)-X) +
+#     mvgamma(c,3)*mvgamma(a+b-c,3)/mvgamma(a,3)/mvgamma(b,3) *
+#     hypergeomPFQ(100, c(c-a,c-b), (3+1)/2+c-a-b, diag(3)-X) *
+#     det(diag(3)-X)^(c-a-b)
+#   expect_equal(o1, o2, tolerance = 1e-4)
+# })
